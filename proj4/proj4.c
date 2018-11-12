2737e3f9c70b5233671063896cac196f66d8c530
jeremy <jsc126@case.edu>
2018-11-12 16:11:43 -0500

    -s works

--- proj4/proj4.c
+++ proj4/proj4.c
@@ -1,36 +1,153 @@
 //Jeremy Chan jsc126
 //EECS 325 Proj 4
 //Oct 24, 2018
+#include <fcntl.h>
+#include <math.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
 
-#DEFINE NOT_PRESENT 0
-#DEFINE PRESENT 1
-#DEFINE ERROR 1
-#DEFINE SUCCESS 1
+#define NOT_PRESENT 0
+#define PRESENT 1
+#define ERROR 1
+#define SUCCESS 1
+#define MAX_PKT_SIZE 1600
+#define READ_MODE "r"
+#define IP_PKT_SIZE 34
 
+struct pkt_info{
+	//captured length of packet
+	unsigned short caplen;
+	double now;
+	unsigned char pkt [MAX_PKT_SIZE];
+	struct ether_header *ethh;
+	struct iphdr *iph;
+	struct tcphdr *tcph;
+	struct udphdr *udph;
+};
+
+struct meta_info{
+	unsigned short caplen;
+	unsigned short ignored;
+	unsigned int secs;
+	unsigned int usecs;
+};
+	
 char mode;
 char *traceFilename;
 
-int error(char *arg){
-	fprintf(stderr, "%s", arg);
+int errexit(char *msg){
+	fprintf(stderr, "%s\n", msg);
 	exit(ERROR);
 }
 
-int summaryMode(){
-	return SUCCESS;
+int isIpPacket(){
+	
+}
+
+double formatTime(int secs, int usecs){
+	double newTime = secs;
+	
+}
+
+unsigned short next_packet(int fd, struct pkt_info *pinfo){
+	struct meta_info meta;
+	int bytes_read;
+
+	memset(pinfo, 0x0, sizeof(struct pkt_info));
+	memset(&meta, 0x0, sizeof(struct meta_info));
+
+	bytes_read = read(fd, &meta, sizeof(meta));
+
+	if(bytes_read == 0)
+		return (0);
+	if(bytes_read < sizeof(meta))
+		errexit("Cannot read meta information");
+	pinfo->caplen = ntohs(meta.caplen);
+
+	double formattedTime = formatTime(meta.secs, meta.usecs);
+	pinfo->now = ntohl(formattedTime);
+	
+	
+	if(pinfo->caplen == 0)
+		return (1);
+	if(pinfo->caplen > MAX_PKT_SIZE)
+		errexit("Packet too big");
+	
+	bytes_read = read(fd, pinfo->pkt, pinfo->caplen);
+	
+	if(bytes_read < pinfo->caplen)
+		errexit("unexpected end of file");
+	if(bytes_read < sizeof(struct ether_header))
+		return (1);
+	
+	pinfo->ethh = (struct ether_header *)pinfo->pkt;
+	pinfo->ethh->ether_type = ntohs(pinfo->ethh->ether_type);
+	
+	
+/*possibly set pinfo->iph and handle byte order */
+	/* possibly set pinfo->udph and handle byte order */
+    /* possibly set pinfo->tcph and handle byte order */
+	return (1);
+}
+
+//returns data from summary mode
+int summaryMode(char *traceFile){
+	struct pkt_info pinfo;
+
+	int pktCount = 0;
+	int ipPkts = 0;
+	
+	int fd = open("409.trace", O_RDONLY);
+
+	if(fd < 0){
+		errexit("Error opening file.");
+	}
+
+	next_packet(fd, &pinfo);
+	if(pinfo.caplen > 0)
+		pktCount++;
+	if(pinfo.caplen >= IP_PKT_SIZE && pinfo.ethh->ether_type == 0x0800){
+		ipPkts++;
+	}
+	
+	double first_time = pinfo.now;
+	double last_time = first_time;
+	
+	while(next_packet(fd, &pinfo)){
+		last_time = pinfo.now;
+		if(pinfo.caplen > 0)
+			pktCount++;
+		if(pinfo.caplen >= IP_PKT_SIZE && pinfo.ethh->ether_type == 0x0800)
+			ipPkts++;
+	}
+
+	close(fd);
+	
+	printf("TIME SPAN: %f - %f\n", first_time, last_time);
+	printf("TOTAL PACKETS: %i\n", pktCount);
+	printf("IP PACKETS: %i\n", ipPkts);
+	
+	exit(SUCCESS);
 }
 
 int lengthMode(){
-	return SUCCESS;
+	exit(SUCCESS);
 }
 
 int packetPrintingMode(){
-	return SUCCESS;
+	exit(SUCCESS);
 }
 
 int trafficMatrixMode(){
-	return SUCCESS;
+	exit(SUCCESS);
 }
 
 int parseargs(int argc, char *argv[]){
@@ -39,7 +156,7 @@ int parseargs(int argc, char *argv[]){
 	int numModeArgs = 0;
 	int tFlag = NOT_PRESENT;
 	
-	while((opt = getopt(argc, argv, "t:slpm"))! = 1){
+	while((opt = getopt(argc, argv, "t:slpm")) != 1){
 		switch(opt){
 		case 't':
 			traceFilename = optarg;
@@ -65,31 +182,33 @@ int parseargs(int argc, char *argv[]){
 	}
 
 	if(tFlag == NOT_PRESENT){
-		error("Trace filename must be specified.\n");
+		errexit("Trace filename must be specified.\n");
 	}
 	if(numModeArgs > 1){
-		error("Only 1 mode can be used.\n");
+		errexit("Only 1 mode can be used.\n");
 	}
 	if(numModeArgs == 0){
-		error("Mode must be specified.\n");
+		errexit("Mode must be specified.\n");
 	}
 
 }
 
 int main(int argc, char *argv []){
-	parseargs(argc, argv);
+	char *filenm = "/shout.trace";
+	summaryMode(filenm);
+	/* parseargs(argc, argv); */
 	
-	if(mode == 's'){
-		summaryMode();
-	}
-	if(mode == 'l'){
-		lengthMode();
-	}
-	if(mode == 'p'){
-		packetPrintingMode();
-	}
-	if(mode == 'm'){
-		trafficMatrixMode();
-	}
+	/* if(mode == 's'){ */
+	/* 	summaryMode(); */
+	/* } */
+	/* if(mode == 'l'){ */
+	/* 	lengthMode(); */
+	/* } */
+	/* if(mode == 'p'){ */
+	/* 	packetPrintingMode(); */
+	/* } */
+	/* if(mode == 'm'){ */
+	/* 	trafficMatrixMode(); */
+	/* } */
 	return 0;
 }
